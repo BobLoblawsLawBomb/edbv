9b73d7eb28e1b7db678a7e29a552a8d43883bf5f
theresa77 <theresa@178.114.92.64.wireless.dyn.drei.com>
2013-11-07 08:52:34 +0100

    connectedComponent überarbeitet, an beispielbild angepasst
    es werden beim beispielbild fast nur mehr kugeln erkannt, kleine
    fragmente vom tisch und kö bleiben, da sie genauso hell sind wie die
    glanzpunkte.

--- connectedComponent.m
+++ connectedComponent.m
@@ -1,21 +1,24 @@
-function [ output_args ] = connectedComponent( img_path )
+function [ result ] = connectedComponent( table_mask )
+%input bild maskiert
 
-img = imread(img_path);
-
-BW = im2bw(img , 0.4); %level womoeglich noch anpassen.
-[L, num] = bwlabel(BW, 4);
+img = table_mask;
+%aus dem bild wird binaerbild, nur die hellsten stellen werden weiß
+%kö und linke und rechte obere Ecke werden auch erkannt 
+BW = im2bw(img , 0.60);
+%nur zur kontrolle 
+imshow(BW);
+%elemente von einander trennen
+[L, num] = bwlabel(BW, 8);
 result = zeros(size(BW,1),size(BW,2), num);
 
+%setzt für jedes Label alle anderen Elemente auf schwarz
 for x = 1:num
     result(:,:,x) = L;
     rx =  result(:,:,x);
     rx(rx<x) = 0;
     rx(rx>x) = 0;
     result(:,:,x) = rx;
-    imshow(rx);
 end;
-
-output_args = result;
 
 end
 

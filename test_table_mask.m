823fdad7dac30d0e4478ddb10bcbe94beeac8848
Maximilian Irro <max@disposia.org>
2013-11-10 22:00:52 +0100

    Pfad jetzt Betriebssystemunabhängig

--- test_table_mask.m
+++ test_table_mask.m
@@ -1,3 +1,6 @@
-img = imread('res/table_test.png');
+path = ['res',filesep,'table_test-6.png'];
+img = imread(path);
 mask = table_mask(img);
 imshow( img .* mask )
+%imshow( rgb2gray(img .* mask) )
+

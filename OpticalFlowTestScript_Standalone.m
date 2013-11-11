6c81943f47776f47714b231f4b03e6184bf23153
Andreas <e1025834@student.tuwien.ac.at>
2013-11-11 21:30:28 +0100

    included optical-flow visualization of velocity per pixel included an Edgedetection Test

--- OpticalFlowTestScript_Standalone.m
+++ OpticalFlowTestScript_Standalone.m
@@ -2,22 +2,28 @@ videoReader = vision.VideoFileReader('res\test.mp4','ImageColorSpace','Intensity
 converter = vision.ImageDataTypeConverter; 
 opticalFlow = vision.OpticalFlow('ReferenceFrameDelay', 1);
 opticalFlow.OutputValue = 'Horizontal and vertical components in complex form';
+opticalFlow.ReferenceFrameSource = 'Input port';
 shapeInserterLine = vision.ShapeInserter('Shape','Lines','BorderColor','Custom', 'CustomBorderColor', 255);
-shapeInserterPoint = vision.ShapeInserter('Shape','Circles','BorderColor','Custom', 'CustomBorderColor', 125);
 videoPlayer = vision.VideoPlayer('Name','Motion Vector');
+
+frame = step(videoReader);
+lastim = step(converter, frame);
 
 while ~isDone(videoReader)
     frame = step(videoReader);
     im = step(converter, frame);
-    of = step(opticalFlow, im);
+    of = step(opticalFlow, im, lastim);
     lines = videooptflowlines(of, 30);
-    %points = lines(1:end, 1:2);
-    %points(1:end, 3) = 0.5;
+    %xv = real(of);
+    %yv = imag(of);
+    vof = abs(of);
+    vimg = mat2gray(vof);
+    imshow(vimg);
     if ~isempty(lines)
-      %out =  step(shapeInserterPoint, im, points); 
       out =  step(shapeInserterLine, im, lines); 
       step(videoPlayer, out);
     end
+    lastim = im;
 end
 
 release(videoPlayer);

ba0c502f9e0ab36e62265995a7e618299066f277
Florian Krall <flo@rockt.at>
2013-11-18 12:01:01 +0100

    Methoden zum Verwalten der Vektormatrix hinzugefügt.

---
+++ add_position_vector_to_matrix.m
@@ -0,0 +1,8 @@
+function [vectorMatrix] = add_position_vector_to_matrix( ball, frame, positionVector, vectorMatrix)
+%add_position_vector_to_matrix Adds the position vector to the matrix
+%   @author Florian Krall
+
+velocityVector = get_velocity_vector(ball, frame, vectorMatrix);
+vectorMatrix(:,:,:,:,ball,frame) = [positionVector(1) positionVector(2) velocityVector(1) velocityVector(2)]; % Position
+
+end
---
+++ add_velocity_vector_to_matrix.m
@@ -0,0 +1,8 @@
+function [vectorMatrix] = add_velocity_vector_to_matrix( ball, frame, velocityVector, vectorMatrix)
+%add_velocity_vector_to_matrix Adds the velocity vector to the matrix
+%   @author Florian Krall
+
+positionVector = get_position_vector(ball, frame, vectorMatrix);
+vectorMatrix(:,:,:,:,ball,frame) = [positionVector(1) positionVector(2) velocityVector(1) velocityVector(2)]; % Velocity
+
+end

51fd1ff3300b827141c4616dfa5e2317c6251d7c
Florian Krall <flo@rockt.at>
2013-11-18 17:18:56 +0100

    moved create_vector_matrix to deprecated

---
+++ deprecated/create_vector_matrix.m
@@ -0,0 +1,8 @@
+function [ vectorMatrix ] = create_vector_matrix( )
+%create_matrix creates the matrix that stores the vectors for position and velocity
+%   @author Florian Krall
+
+%create the vector matrix
+vectorMatrix = [0 0 0 0]; % Position and Velocity of Ball 1 in Frame 1
+
+end

a9c2037f75b83362da5526aa9db4e74c2e09574c
Maximilian Irro <max@disposia.org>
2013-12-03 11:53:55 +0100

    Abstracte Bucket Class hinzugefügt

---
+++ abstractBucket.m
@@ -0,0 +1,15 @@
+classdef (Abstract) abstractBucket
+    %UNTITLED2 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties (Abstract, Constant)
+        hueMin;
+        hueMax;
+        satMin;
+        satMax;
+        valMin;
+        valMax;
+    end
+    
+end
+

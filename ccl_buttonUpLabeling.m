afc328f9721a959690e5c8a5013c13c47ba915f2
Theresa Fröschl <theresa.froeschl@gmail.com>
2013-12-03 16:21:31 +0100

    Kommentare in den ccl-funktionen hinzugefügt

--- ccl_buttonUpLabeling.m
+++ ccl_buttonUpLabeling.m
@@ -1,4 +1,8 @@
 function [ runlengthTable ] = ccl_buttonUpLabeling(runlengthTable )
 
+% labeling von unten nach oben
+% für jede Zeichenkette in der runlengthTable wird überprüft, ob die
+% Elemente der Zeichenkette darunter an anderes gelabeltes Element haben,
+% dass ein kleineres Label hat.
 
 end
--- ccl_labelNormalisation.m
+++ ccl_labelNormalisation.m
@@ -1,4 +1,9 @@
 function [ components_img, num ] = ccl_labelNormalisation(runlengthTable, bw_img)
 
+% hier werden die Werte der Labels zuerst normalisiert, sodass diese eine
+% durchgehende Nummerierung haben,
+% anschließend wird das Ursprungsbild anhand dieser Labels geändert
+% zurückgegeben wird das gelabelte Logical-Bild und die Anzahl der darin
+% enthaltenen Components
 
 end

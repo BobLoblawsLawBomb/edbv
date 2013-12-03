bce82092e046495764b2ac8afdf8213bcea92b4d
Maximilian Irro <max@disposia.org>
2013-12-03 21:33:50 +0100

    Merke: Setzt man in WhiteBucket die Property satMax=0.35 und Constant, so ist ihr Wert zur Laufzeit = 0.05 Logisch, oder? …oh wait

--- dominantBucketColor2.m
+++ dominantBucketColor2.m
@@ -1,16 +1,22 @@
-function [ dominantColor ] = dominantBucketColor2( img )
+function [ dominantBucket ] = dominantBucketColor2( img )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 %   @author Maximilian Irro
 
-    buckets = [ BlackBucket,    0;
-                BlueBucket,     0;
-                BrownBucket,    0;
-                GreenBucket,    0;
-                PinkBucket,     0;
-                RedBucket,      0;
-                WhiteBucket,    0;
-                YellowBucket,   0];
+    bucketList = {  WhiteBucket(), GreenBucket(), BlueBucket(), BrownBucket(),  
+                    PinkBucket(), BlackBucket(), YellowBucket(), RedBucket()};
+            
+    bucketStack = [0,0,0,0,0,0,0,0];
+            
+%     buckets = cell(8,2);
+%     buckets(1,:) = { BlackBucket(), 0};
+%     buckets(2,:) = { BlueBucket(),  0};
+%     buckets(3,:) = { BrownBucket(), 0};
+%     buckets(4,:) = { GreenBucket(), 0};
+%     buckets(5,:) = { PinkBucket(),  0};
+%     buckets(6,:) = { RedBucket(),   0};
+%     buckets(7,:) = { WhiteBucket(), 0};
+%     buckets(8,:) = { YellowBucket(),0};
             
     tableMask = im2bw(img,0.000001);
 
@@ -21,38 +27,48 @@ function [ dominantColor ] = dominantBucketColor2( img )
             
     for n=1:7 % there are 7 buckets
         
-        bucket = buckets(n,1);
+        bucket = bucketList{n};
         
         % make a mask that lets only pass those pixels 
         % within the specified bucket hue range
-        if isa(bucket,RedBucket) % red bucket has different evaluation due to hsv hue definition
-            hueMask = hue>bucket.hueMin | hue<bucket.hueMax; 
+        if isa(bucket, 'RedBucket') % red bucket has different evaluation due to hsv hue definition
+            hueMask = hue>=bucket.hueMin | hue<=bucket.hueMax; 
         else
-            hueMask = hue>bucket.hueMin & hue<bucket.hueMax;   
+            hueMask = hue>=bucket.hueMin & hue<=bucket.hueMax;   
         end
+        imshow(hueMask)
         
         % make a mask that lets only pass those pixels 
         % within the specified bucket saturation range
-        satMask = sat>bucket.satMin & sat<bucket.satMax;
+        sat(sat>0)
+        imshow(sat)
+        bucket.satMax
+        satMask = sat<=bucket.satMax;
+        %satMask = sat>=bucket.satMin & sat<=bucket.satMax;
+        imshow(satMask)
         
         % make a mask that lets only pass those pixels 
         % within the specified bucket value range
-        valMask = val>bucket.valMin & sat<bucket.valMax;
+        valMask = val>=bucket.valMin & sat<=bucket.valMax;
+        imshow(valMask)
         
         % filter every pixel that passes all 3 HSV filters
         bucketPixels = tableMask .* hueMask .* satMask .* valMask;
+        class(bucket)
+        imshow(bucketPixels)
         
         % count all pixels that are not 0
         % those match the HSV specifications of this color bucket
         pixelCount =  nnz( bucketPixels );
         
         % safe the pixel count for later comparison
-        buckets(n,2) = pixelCount;
+        bucketStack(n) = pixelCount;
         
     end
     
-    ??? [colorElementCount, bucketIndex] = max(buckets(:,2));
+    % find the bucket with the most classified pixels
+    [maxPixelCount, bucketIndex] = max(bucketStack(:));
 
-    dominantColor = hsv (oder rgb?) von dominater farbe
+    dominantBucket = bucketList{bucketIndex};
 end
 

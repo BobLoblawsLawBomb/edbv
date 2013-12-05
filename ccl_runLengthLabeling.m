10f0bedf9392c18e9915aeceba9ab2335e4289c1
Theresa Fröschl <theresa.froeschl@gmail.com>
2013-12-05 15:32:20 +0100

    Implementierung der Funkton ccl_runLengthLabeling + 2 Bild zum testen

--- ccl_runLengthLabeling.m
+++ ccl_runLengthLabeling.m
@@ -1,6 +1,9 @@
 function [ runlengthTable ] = ccl_runLengthLabeling( bw_img )
 % n Reihen, 4 Spalten: Aktuelle Reihe, start, ende, Label
 
+img = imread('res/connected1.png');
+bw_img = im2bw(img, 0.50);
+
 % bw_img Zeilenweise durchgehen und nach Zeichenketten suchen.
 % für jede Zeichenkette den Anfangswert und den Endwert in der
 % runlengthTable speichern
@@ -9,5 +12,68 @@ function [ runlengthTable ] = ccl_runLengthLabeling( bw_img )
 % für jeden Element der Zeichenkette überprüfen, ob das Element der in
 % darüberliegenden Zeile bereits gelabelt wurde.
 
+% Tabelle der Zeichenketten der Zeilen
+runlengthTable = cell(1);
+
+count = 1;
+
+for x = 1:size(bw_img,2)
+     % x-te zeile aus tabelle holen
+     currentRow = bw_img(x,:);
+      
+     % Indizes der nonzero-Elemente des Zeilenvektors
+     indices = find(currentRow);
+     cursor_row = 1;
+     cursor_ind = 1;
+     
+     if ~isempty(indices)
+         while cursor_row <= length(currentRow) && cursor_ind <= length(indices)
+             % Start der Zeichenkette und Cursor für Reihe und Index-Vektor setzen
+             start_string = indices(cursor_ind);
+             cursor_row = start_string + 1;
+             cursor_ind = cursor_ind + 1;
+             
+             % vorläufiges Label
+              [~, num] = size(runlengthTable);
+             if ~isempty(runlengthTable{1})
+                 label = num + 1;
+             else
+                 label = 1;
+             end
+             
+             % Schleife läuft, bis das Ende der Zeichenkette erreicht ist
+             while (cursor_row <= length(currentRow)) && (currentRow(cursor_row) ~= 0)
+                 if x>1
+                    bool = 1;
+                    % für jede Zeichenkette eine Zeile darüber wird
+                    % überprüft, ob die Zeichenkette direkt über dem
+                    % aktuellen Element liegt
+                     while bool && num > 0
+                         if ~isempty(runlengthTable{1}) && runlengthTable{1,num}(1) == (x-1)
+                             if (runlengthTable{1,num}(2)<=cursor_row) && (runlengthTable{1,num}(3)>=cursor_row)
+                                 if label > runlengthTable{1,num}(4)
+                                    label = runlengthTable{1,num}(4);
+                                 end
+                             end
+                         else
+                             bool = 0;
+                         end
+                         num = num - 1;
+                     end
+                 end
+                 
+                 cursor_row = cursor_row + 1;
+                 cursor_ind = cursor_ind + 1;
+             end
+             
+             % Ende der Zeichenkette angeben
+             end_string = cursor_row - 1;
+             
+             % codierte Zeichenkette in Tabelle speichern
+             runlengthTable{count} = [x start_string end_string label];
+             count = count + 1;
+         end
+     end
+end
 
 end

0ddeb2b5fb4c75df55c8086c18459390495c2edc
Maximilian Irro <max@disposia.org>
2013-12-08 20:21:32 +0100

    test_colorClassification erstellt, um die Funktion in Betrieb nehmen zu können

---
+++ test_colorClassification.m
@@ -0,0 +1,9 @@
+img = imread('res/table_test-1.png');
+mask = table_mask(img);
+image = img .* mask;
+
+imshow(image)
+
+[BWComponents, ColorComponents] = connectedComponent(image);
+
+[ red, white, black, green, blue, yellow, pink, brown ] = colorClassification( colorComponents );

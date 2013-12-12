cf17221498768c7c109a2a967204f47565b170d4
AndreasMR <e1025834@student.tuwien.ac.at>
2013-12-12 14:07:52 +0100

    Arbeiten an OpticalFlow-Analyse für Tracking.

--- mainfunction.m
+++ mainfunction.m
@@ -7,7 +7,7 @@ function [ output_args ] = mainfunction()%argument:  video_path
 % relative pfade scheinen mit dem videfilereader auf
 % unix systeme nicht zu funktionieren, siehe http://blogs.bu.edu/mhirsch/2012/04/matlab-r2012a-linux-computer-vision-toolbox-bug/
 %
-video_path = [pwd,filesep,'res',filesep,'test_hd_4.mp4'];
+video_path = [pwd,filesep,'res',filesep,'test_hd_4_short.mp4'];
 
 
 
@@ -165,6 +165,66 @@ while ~isDone(videoReader)
             compPosition(:, :, k, frameNo) = getPositionOfComponent(resultBW{k});
         end
         
+        %berechne optical-flow komponenten
+        angleVariance = 5; %ähnlichkeitsumgebung in grad
+        velocityThreshold = 0.001;
+        
+        [ofw, ofh] = size(of);
+        ofAngle = angle(of);
+        ofVelocity = abs(of);
+        ofAngleListAll = reshape(ofAngle, ofw*ofh, 1);
+        ofVelocityListAll = reshape(ofVelocity, ofw*ofh, 1);
+        
+        ofAngleListTrim = ofAngleListAll(ofVelocityListAll(:) > velocityThreshold);
+        
+        ofAngleListSorted = sort(radtodeg(ofAngleListTrim)); % von bei pos 1 weg: -180 < angle <= 180
+        ofAngleUniqueList = unique(ofAngleListSorted);
+        ofAngleCountList = zeros(size(ofAngleUniqueList));
+        
+        disp([size(ofAngleUniqueList) size(ofAngleListSorted)]);
+        
+        for k = 1 : size(ofAngleUniqueList)
+            anglek = ofAngleUniqueList(k);
+            %zähle ähnliche richtungen in gewissem bereich
+            edge1 = anglek - angleVariance;
+            if edge1 <= -180
+                edge1 = edge1 + 360;
+            end
+            
+            edge2 = anglek + angleVariance;
+            if edge2 > 180
+                edge2 = edge2 - 360;
+            end
+            
+            if edge1 > edge2   % achtung: der zu betrachtende bereich springt von 180 zu -180
+                n = histc(ofAngleListSorted, [-inf edge2 edge1 inf]);
+                ofAngleCountList(k) = n(1) + n(3);
+            else
+                n = histc(ofAngleListSorted, [edge1 edge2]);
+                ofAngleCountList(k) = n(1);
+            end
+        end
+        
+        fig3 = figure(3);
+        of_max = max(max(max(abs(of))));
+        color_of = double(repmat(output_vmask, [1 1 3]));
+        color_of(:,:,1) = 0.5 + ofAngle/(2*pi);
+        color_of(:,:,2) = 1;
+        color_of(:,:,3) = abs(of)/of_max;
+        imshow(hsv2rgb(color_of));
+        
+        print(fig3, '-dpng', [pwd filesep 'results' filesep 'OF_Color_' num2str(frameNo) '.png'])
+        
+        [pks, locs] = findpeaks(ofAngleCountList, 'MINPEAKHEIGHT', 1, 'THRESHOLD', 3);
+        
+        fig2 = figure(2);
+        bar(ofAngleUniqueList, ofAngleCountList, 'BarWidth', 1);
+        ylim([0 2000])
+        hold on;
+        plot(ofAngleUniqueList(locs), pks + 0.05, 'k^', 'markerfacecolor', [1 0 0]);
+        hold off;
+        %print(fig2, '-dpng', [pwd filesep 'results' filesep 'Vert_' num2str(velocityThreshold) '_' num2str(angleVariance) filesep 'Vert_' num2str(frameNo) '.png'])
+        
         %berechne geschwindigkeiten der komponenten
         for k = 1 : length(resultBW(:))
             [vx, vy, vmask] = calcComponentVelocity(of, im, compPosition(:, :, k, frameNo), 20);
@@ -228,6 +288,7 @@ while ~isDone(videoReader)
         maxv = max(max(max(output_mask)));
         output_mask = output_mask./maxv;
         
+        figure(1)
         imshow(output_mask);
         
         %For movie creation at the end

6157f30ac71b174d0ce4daf2b44a163fbf43cad9
Maximilian Irro <max@disposia.org>
2013-12-14 18:25:43 +0100

    Massive Glättungen im Grünkanals des L*a*b Raum wirken sich kaum auf das Gesamtbild aus.

--- test_tableSmoothing.m
+++ test_tableSmoothing.m
@@ -13,6 +13,8 @@ rg_chroma = lab(:,:,2); % hell bedeutet mehr rot, dunkel mehr gruen
 % anderen
 rg_bw = imcomplement(im2bw(rg_chroma,0.45));
 
+imshow(rg_bw);
+
 % hier ist alles ungleich 0, was nicht der tisch ist
 table_red = rg_chroma .* uint8(imcomplement(rg_bw));
 %imshow(table_red)
@@ -26,11 +28,14 @@ table_green = rg_chroma .* uint8(rg_bw);
 
 % jetzt zeichnen wir den gruenen bereich weich und eliminieren hoffentlich
 % die fehler
-h = fspecial('gaussian');
+imshow(table_green);
+%h = fspecial('gaussian', 40, 3);
+h = fspecial('average', 50);
 table_green = imfilter(table_green,h);
+table_green = table_green .* uint8(rg_bw);
 
 % BREAKPOINT
-%imshow(table_green)
+imshow(table_green);
 
 % nun kombinieren wir wieder alles auf dem tisch mit der modifizierten
 % tischflaeche
